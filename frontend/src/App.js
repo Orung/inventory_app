8e4408cdfe5a447ba78e0d606e2b497599abc561
Promise Orung <magnificentorung@gmail.com>
2023-03-30 17:39:06 +0100

    FT: Implement design for dashboard sidebar and topbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/Signup";
 import Home from "./pages/Home";
-// import Admin from "./pages/Admin";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<SignUp />} />
-          {/* <Route path="/admin"  element={<Admin />} /> */}
+          <Route path="/admin"  element={<Admin />} />
           <Route element={<Home />} />
         </Routes>
       </BrowserRouter>
